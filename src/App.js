514ff12eb8a8f4069bd5f1ed22ba9d897d0400e8
ccp84 <ccp84@icloud.com>
2023-04-25 13:46:41 +0000

    py>begin react routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import './App.css';
-import NavBar from './components/NavBar';
+import styles from "./App.module.css";
+import NavBar from "./components/NavBar";
+import { Container } from "react-bootstrap";
+import { Switch, Route } from "react-router-dom/cjs/react-router-dom.min";
 
 function App() {
-  return (
-    <div className="App">
-      <NavBar />
-    </div>
-  );
+    return (
+        <div className={styles.App}>
+            <NavBar />
+            <Container className={styles.Main}>
+              <Switch>
+                <Route exact path="/" render={() => <h1>Home Page</h1>} />
+                <Route exact path="/signin" render={() => <h1>Sign In</h1>} />
+                <Route exact path="/signup" render={() => <h1>Sign Up</h1>} />
+                <Route render={() => <h1>Page Not Found</h1>} />
+              </Switch>
+            </Container>
+        </div>
+    );
 }
 
 export default App;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,31 +1,34 @@
 import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
+import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
 import logo from "../assets/logo.ico";
 
 const NavBar = () => {
     return (
-      <Navbar expand="md" fixed="top">
-        <Container>
-          <Navbar.Brand>
-            <img src={logo} alt="logo" height="45" />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ml-auto text-left">
-              <Nav.Link>
-                <i className="fas fa-home"></i>Home
-              </Nav.Link>
-              <Nav.Link>
-                <i className="fas fa-sign-in-alt"></i>Sign in
-              </Nav.Link>
-              <Nav.Link>
-                <i className="fas fa-user-plus"></i>Sign up
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+        <Navbar expand="md" fixed="top">
+            <Container>
+                <NavLink to="/">
+                    <Navbar.Brand>
+                        <img src={logo} alt="logo" height="45" />
+                    </Navbar.Brand>
+                </NavLink>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ml-auto text-left">
+                        <NavLink to="/">
+                            <i className="fas fa-home"></i>Home
+                        </NavLink>
+                        <NavLink to="/signin">
+                            <i className="fas fa-sign-in-alt"></i>Sign in
+                        </NavLink>
+                        <NavLink to="signup">
+                            <i className="fas fa-user-plus"></i>Sign up
+                        </NavLink>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
-  };
+};
 
 export default NavBar;
