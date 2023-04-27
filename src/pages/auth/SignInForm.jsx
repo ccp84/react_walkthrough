e2f93d30f7915af63097f44e30eaafa6a6a7d766
ccp84 <ccp84@icloud.com>
2023-04-27 12:23:23 +0000

    jsx>create sign in form

--- src/pages/auth/SignInForm.jsx
+++ src/pages/auth/SignInForm.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Form from "react-bootstrap/Form";
 import Alert from "react-bootstrap/Alert";
@@ -13,32 +13,80 @@ import { Link } from "react-router-dom";
 import styles from "../../styles/SignInUpForm.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
+import axios from "axios";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 function SignInForm() {
-	//   Add your component logic here
+	const [signInData, setSignInData] = useState({
+		username: "",
+		password: "",
+	});
+	const { username, password } = signInData;
+	const [errors, setErrors] = useState({});
+	const history = useHistory();
+
+	const handleChange = (event) => {
+		setSignInData({
+			...signInData,
+			[event.target.name]: event.target.value,
+		});
+	};
+
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+		try {
+			await axios.post("dj-rest-auth/login/", signInData);
+			history.push("/");
+		} catch (err) {
+			setErrors(err.response?.data);
+		}
+	};
 
 	return (
 		<Row className={styles.Row}>
 			<Col className="my-auto p-0 p-md-2" md={6}>
 				<Container className={`${appStyles.Content} p-4 `}>
 					<h1 className={styles.Header}>sign in</h1>
-					<Form>
-						<Form.Group controlId="formBasicEmail">
-							<Form.Label>Email address</Form.Label>
-							<Form.Control type="email" placeholder="Enter email" />
-							<Form.Text className="text-muted">
-								We'll never share your email with anyone else.
-							</Form.Text>
+					<Form onSubmit={handleSubmit}>
+						<Form.Group controlId="username">
+							<Form.Label>Username</Form.Label>
+							<Form.Control
+								type="text"
+								placeholder="Username"
+								name="username"
+								value={username}
+								onChange={handleChange}
+							/>
 						</Form.Group>
+						{errors.username?.map((message, idx) => (
+							<Alert variant="warning" key={idx}>
+								{message}
+							</Alert>
+						))}
 
-						<Form.Group controlId="formBasicPassword">
+						<Form.Group controlId="Password">
 							<Form.Label>Password</Form.Label>
-							<Form.Control type="password" placeholder="Password" />
+							<Form.Control
+								type="password"
+								placeholder="Password"
+								name="password"
+								value={password}
+								onChange={handleChange}
+							/>
 						</Form.Group>
-						<Form.Group controlId="formBasicCheckbox">
-							<Form.Check type="checkbox" label="Check me out" />
-						</Form.Group>
-						<Button variant="primary" type="submit">
+						{errors.password?.map((message, idx) => (
+							<Alert variant="warning" key={idx}>
+								{message}
+							</Alert>
+						))}
+
+						{errors.non_field_errors?.map((message, idx) => (
+							<Alert variant="warning" className="mt-3" key={idx}>
+								{message}
+							</Alert>
+						))}
+
+						<Button className={btnStyles.Button} type="submit">
 							Submit
 						</Button>
 					</Form>
